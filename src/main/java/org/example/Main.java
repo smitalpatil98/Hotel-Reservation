8b82e62fa69838452bf820256d4a3a7a351b87b6
smitalpatil98 <patilsmital.1101998gmail.com>
2024-06-28 14:28:51 +0530

    file created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,36 +7,68 @@ class Hotelreservation
 {
     String name;
     long hotel_Regular_Price;
+    String start_date;
+    String end_date;
 
-    Hotelreservation(String Name, long Hotel_Regular_Price)
+    Hotelreservation(String Name, long Hotel_Regular_Price, String Start_Date , String End_Date)
     {
         this.name = Name;
         this.hotel_Regular_Price = Hotel_Regular_Price;
+        this.start_date = Start_Date;
+        this.end_date  = End_Date;
     }
-
     @Override
     public String toString()
     {
-        return "Hotel reservation{" + "name='" + name + '\'' + ", hotel_Regular_Price=" + hotel_Regular_Price + '}';
+        return "Hotelreservation{" +
+                "end_date='" + end_date + '\'' +
+                ", name='" + name + '\'' +
+                ", hotel_Regular_Price=" + hotel_Regular_Price +
+                ", start_date='" + start_date + '\'' +
+                '}';
     }
 }
-
  public class Main
  {
     static List<Hotelreservation> hotelreservationList = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
-    static Boolean addHotel(String Name, long Hotel_Regular_Price)
-    {
-        Hotelreservation hot = new Hotelreservation(Name, Hotel_Regular_Price);
-        hotelreservationList.add(hot);
-        if(Name.equals(hot.name) && Hotel_Regular_Price == hot.hotel_Regular_Price)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-}
+     static String cheapesthotel(String Start_Date,String End_Date)
+     {
+         long min = Integer.MAX_VALUE;
+         String name = null;
+
+         for (Hotelreservation hotels : hotelreservationList)
+         {
+             if (Start_Date.equals(hotels.start_date) && End_Date.equals(hotels.end_date))
+             {
+                 if (hotels.hotel_Regular_Price < min)
+                 {
+                     min = hotels.hotel_Regular_Price;
+                     name = hotels.name;
+                 }
+             }
+         }
+         return name;
+     }
+     static Boolean addHotel(String name, long hotel_Regular_Price,String start_date,String end_date)
+     {
+         Hotelreservation hot = new Hotelreservation(name, hotel_Regular_Price, start_date, end_date );
+         hotelreservationList.add(hot);
+         if(name.equals(hot.name) && hotel_Regular_Price == hot.hotel_Regular_Price) {
+             return true;
+         }
+         else
+         {
+             return false;
+         }
+     }
+     public static void main(String [] args)
+     {
+         Main m = new Main();
+         m.addHotel("Radisson Blu",30000,"20-may-2002","21-july-2002");
+         m.addHotel("InterContinental Hotels & Resorts",6500,"20-may-2002","21-july-2002");
+         m.addHotel("Hyatt Regency",6000,"20-jan-2002","11-feb-2002");
+         m.addHotel("JW Marriott International",4000,"22-nov-2002","22-dec-2002");
+     }
+ }
--- src/test/java/org/example/HotelreservationTest.java
+++ src/test/java/org/example/HotelreservationTest.java
@@ -7,11 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HotelreservationTest
 {
+        Main m = new Main();
         @Test
         public void testAdd()
         {
-        Main m = new Main();
-        Assert.assertTrue(m.addHotel("Oberoi",20000));
-        Assert.assertTrue( m.addHotel("TajLand Ends Delhi",4000));
+                Assert.assertTrue(m.addHotel("Radisson Blu",30000,"20-may-2002","21-july-2002"));
+                Assert.assertTrue(m.addHotel("InterContinental Hotels & Resorts",6500,"20-may-2002","21-july-2002"));
+                Assert.assertTrue(m.addHotel("Hyatt Regency",6000,"20-jan-2002","11-feb-2002"));
+                Assert.assertTrue(m.addHotel("JW Marriott International",4000,"22-nov-2002","22-dec-2002"));
+        }
+
+        @Test
+        public void testcheapesthotel()
+        {
+                m.addHotel("Radisson Blu",30000,"20-may-2002","21-july-2002");
+                m.addHotel("InterContinental Hotels & Resorts",6500,"20-may-2002","21-july-2002");
+                m.addHotel("Hyatt Regency",6000,"20-jan-2002","11-feb-2002");
+                m.addHotel("JW Marriott International",4000,"22-nov-2002","22-dec-2002");
+                String str = m.cheapesthotel("22-nov-2002", "22-dec-2002");
+                Assert.assertEquals("JW Marriott International", str );
+                //System.out.println(str);
         }
 }
